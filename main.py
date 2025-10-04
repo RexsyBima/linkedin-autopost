9ca37db8703e786c67d7fd9f794d5540fbb7c00f
rexsybimatw <rexsy.bimq12@gmail.com>
2025-10-04 11:58:58 +0700

    added .env.example

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Any
+from pprint import pprint as print
 import requests
 from dotenv import load_dotenv
 import os
@@ -6,19 +7,38 @@ import os
 load_dotenv()
 
 
-def get_userinfo(secret: str) -> tuple[dict[str, Any], str]:
+def _get_userinfo(secret: str) -> tuple[dict[str, Any], str]:
     url = "https://api.linkedin.com/v2/userinfo"
     headers = {"Authorization": f"Bearer {secret}"}
     response_json = requests.get(url, headers=headers).json()
     return response_json, response_json["sub"]
 
 
-def post_content(content: str, profile_id: str, access_token: str):
+def _register_media(profile_id: str, access_token: str):
     headers = {
         "Content-Type": "application/json",
         "X-Restli-Protocol-Version": "2.0.0",
         "Authorization": "Bearer " + access_token,
     }
+
+    post_data = {
+        "registerUploadRequest": {
+            "recipes": ["urn:li:digitalmediaRecipe:feedshare-image"],
+            "owner": "urn:li:person:" + profile_id,
+            "serviceRelationships": [
+                {
+                    "relationshipType": "OWNER",
+                    "identifier": "urn:li:userGeneratedContent",
+                }
+            ],
+        }
+    }
+    url = "https://api.linkedin.com/v2/assets?action=registerUpload"
+    response = requests.post(url, headers=headers, json=post_data)
+    return response
+
+
+def post_content_no_image(content: str, profile_id: str, access_token: str):
     url = "https://api.linkedin.com/v2/ugcPosts"
 
     headers = {
@@ -42,18 +62,66 @@ def post_content(content: str, profile_id: str, access_token: str):
     return response
 
 
-def main():
+def _post_content_image(
+    content: str, profile_id: str, access_token: str, media_asset: str
+):
+    url = "https://api.linkedin.com/v2/ugcPosts"
+    headers = {
+        "Content-Type": "application/json",
+        "X-Restli-Protocol-Version": "2.0.0",
+        "Authorization": "Bearer " + access_token,
+    }
+    post_data = {
+        "author": f"urn:li:person:{profile_id}",
+        "lifecycleState": "PUBLISHED",
+        "specificContent": {
+            "com.linkedin.ugc.ShareContent": {
+                "shareCommentary": {"text": content},
+                "shareMediaCategory": "IMAGE",
+                "media": [
+                    {
+                        "status": "READY",
+                        "description": {"text": "Center stage!"},
+                        "media": media_asset,
+                        "title": {"text": "LinkedIn Talent Connect 2021"},
+                    }
+                ],
+            }
+        },
+        "visibility": {"com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"},
+    }
+    response = requests.post(url, headers=headers, json=post_data)
+    return response
+
+
+def post_personal_branding_with_image(
+    image_path: str | None = None, content: str | None = None
+):
+    if image_path is None or content is None:
+        raise Exception("parameter of image_path and content must be filled bruh")
     user_secret = os.getenv("USER_SECRET")
-    assert user_secret is not None, "USER_SECRET env is empty, fill it up"
-    client_id = os.getenv("CLIENT_ID")
-    client_secret = os.getenv("CLIENT_SECRET")
-    user_info, profile_id = get_userinfo(user_secret)
-    response = post_content(
-        "Hellow world from linkedin API because why not", profile_id, user_secret
+    assert user_secret is not None, "user secret must be filled"
+    _, profile_id = _get_userinfo(user_secret)
+    response = _register_media(profile_id, user_secret)
+    asset = response.json()["value"]["asset"]
+    url = response.json()["value"]["uploadMechanism"][
+        "com.linkedin.digitalmedia.uploading.MediaUploadHttpRequest"
+    ]["uploadUrl"]
+
+    headers = {"Authorization": f"Bearer {os.getenv('USER_SECRET')}"}
+    with open(image_path, "rb") as f:
+        response = requests.post(url, headers=headers, data=f)
+    response = _post_content_image(
+        content=content,
+        profile_id=profile_id,
+        access_token=user_secret,
+        media_asset=asset,
     )
+
     print(response)
-    print(user_info, profile_id, sep="\n\n")
+    print(response.status_code)
+    print(response.json())
 
 
 if __name__ == "__main__":
-    main()
+    post_personal_branding_with_image()
